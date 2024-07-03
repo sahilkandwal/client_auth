e831d483c3feabcf8725f08ad7b15e875a2eafc9
abhishek-tec123 <kumartinkesh32@gmail.com>
2024-07-03 15:32:16 +0530

    second

--- app/app_server.py
+++ app/app_server.py
@@ -354,3 +354,6 @@ def get_data(reference_id):
 # Register Blueprints
 app.register_blueprint(credentials_bp)
 app.register_blueprint(tokens_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- app/wsgi.py
+++
@@ -1,4 +0,0 @@
-from app_server import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
